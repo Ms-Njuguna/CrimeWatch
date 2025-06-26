4603272f44e03d60ce0242ec0b1fa73dd6c9a2af
Patricia <patricianjuguna084@gmail.com>
2025-06-26 17:10:35 +0300

    Create dynamic fugitive cards with name, image, and details

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 
 //fetch fugitive data from the FBI most wanted public API
 
-fetch('https://api.fbi.gov/wanted/v1/list')
-.then(res => res.json())
-.then(data => {
+function fetchFugitiveData() {
+    fetch('https://api.fbi.gov/wanted/v1/list')
+    .then(res => res.json())
+    .then(data => {
 
-    const fugitives = data.items;
-    console.log(fugitives);
-    
+        const fugitives = data.items;
+        console.log(fugitives);
+
+        displayFugitives(fugitives);
+    })
+}
+
+
+//the idea is to create fugitive cards that show name, image, and details
+
+function displayFugitives(fugitives) {
     const mainSection = document.querySelector('main')
-    fugitives.forEach(fugitive => {
-        const fugitiveTitle = document.createElement('p')
-        fugitiveTitle.textContent = `${fugitive.title}`
 
-        mainSection.appendChild(fugitiveTitle)
+    fugitives.forEach(fugitive => {
+        const fugitiveCard = document.createElement('div')
+        fugitiveCard.innerHTML = `
+            <img src="${fugitive.images[0]}" alt="${fugitive.title}">
+            <div>
+                <h2>${fugitive.title}</h2>
+                <p>${fugitive.description}</p>
+            </div>
+            <div>
+                <p>${fugitive.warning_message}</p>
+                <p>${fugitive.reward_text}</p>
+            </div>
+            <button>Report Sighting</button>
+        `
+
+        mainSection.appendChild(fugitiveCard)
+        console.log(fugitiveCard);
     });
-})
+}
